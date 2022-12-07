7ffbe92fc0856fb374d3297dd56416522aa5b12f
Leo Spratt <enchant97@users.noreply.github.com>
2022-12-07 16:29:36 +0000

    add adjustable mtu with env value

--- prototypes/proto-1/main.go
+++ prototypes/proto-1/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -10,11 +11,23 @@ func main() {
 
 	mtu := uint32(512)
 
+	if value, isSet := os.LookupEnv("NET_MTU"); isSet {
+		value, err := strconv.ParseUint(value, 10, 32)
+		if err != nil {
+			log.Fatalln("NET_MTU invalid")
+		}
+		mtu = uint32(value)
+	}
+
+	log.Println("MTU set as =", mtu)
+
 	if args[1] == "server" {
+		log.Println("starting server...")
 		server(args[2], mtu)
 	}
 	if args[1] == "client" {
+		log.Println("starting client...")
 		client(args[2], mtu, args[3])
 	}
-	fmt.Println("done")
+	log.Println("done")
 }
